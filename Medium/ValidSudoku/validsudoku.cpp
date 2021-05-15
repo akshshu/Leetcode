09505170f817eb2e9fc186c209ff534ad52bab5d
akshshu <akshshu@gmail.com>
2021-05-15 16:04:51 +0530

    validsudoku.cpp added

---
+++ Medium/ValidSudoku/validsudoku.cpp
@@ -0,0 +1,54 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool isValidSudoku(vector<vector<char>> &board)
+    {
+        vector<vector<int>> rowcheck(9, vector<int>(9, 0));
+        vector<vector<int>> colcheck(9, vector<int>(9, 0));
+        vector<vector<int>> boxcheck(9, vector<int>(9, 0));
+        int boxno;
+        for (int i = 0; i < 9; i++)
+        {
+            boxno = ((i / 3) * 3) - 1;
+
+            for (int j = 0; j < 9; j++)
+            {
+                if (j % 3 == 0)
+                {
+                    boxno++;
+                }
+                if (board[i][j] != '.')
+                {
+                    if (rowcheck[i][((int)board[i][j]) - 49] == 0 && colcheck[j][((int)board[i][j]) - 49] == 0 && boxcheck[boxno][((int)board[i][j]) - 49] == 0)
+                    {
+                        rowcheck[i][((int)board[i][j]) - 49] = 1;
+                        colcheck[j][((int)board[i][j]) - 49] = 1;
+                        boxcheck[boxno][((int)board[i][j]) - 49] = 1;
+                    }
+                    else
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<vector<char>> board{
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+    auto ans = s.isValidSudoku(board);
+    return 0;
+}
