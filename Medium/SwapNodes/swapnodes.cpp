b3affe3851f74337a3278dc50f4785a0f85c0ff9
akshshu <akshshu@gmail.com>
2021-05-17 22:36:22 +0530

    Update swapnodes.cpp

--- Medium/SwapNodes/swapnodes.cpp
+++ Medium/SwapNodes/swapnodes.cpp
@@ -1,6 +1,13 @@
-
-#include <bits/stdc++.h>
-using namespace std;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution
 {
 public:
@@ -11,16 +18,18 @@ public:
             return head;
         }
         ListNode *ptr = head;
-        ListNode *hlpptr = head;
+        ListNode *hlpptr = head->next;
+        int i = 0;
         while (hlpptr != nullptr)
         {
+            if (i % 2 == 0)
+            {
+                swap(ptr->val, hlpptr->val);
+            }
+            i++;
+            ptr = ptr->next;
             hlpptr = hlpptr->next;
         }
+        return head;
     }
 };
-int main()
-{
-    Solution s;
-    auto ans = s.func();
-    return 0;
-}
