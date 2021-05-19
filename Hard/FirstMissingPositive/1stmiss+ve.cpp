95f4463acd31d8333e0f5f57f8be92ef213738c6
akshshu <akshshu@gmail.com>
2021-05-19 08:32:25 +0530

    1stmiss+ve.cpp added

---
+++ Hard/FirstMissingPositive/1stmiss+ve.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        nums.push_back(0);
+        int m = nums.size();
+
+        for (auto &x : nums)
+        {
+            if (x <= 0)
+            {
+                x = nums.size() + 4;
+            }
+        }
+        for (int i = 0; i < m; i++)
+        {
+            if (abs(nums[i]) < m)
+            {
+                nums[abs(nums[i])] = -1 * abs(nums[abs(nums[i])]);
+            }
+        }
+        for (int i = 1; i < m; i++)
+        {
+            if (nums[i] > 0)
+                return i;
+        }
+        return nums.size();
+    }
+};
+int main()
+{
+    Solution s;
+    auto ans = s.func();
+    return 0;
+}
---
+++ Hard/FirstMissingPositive/Readme.md
@@ -0,0 +1,2 @@
+## Given an unsorted integer array nums, find the smallest missing positive integer. <br> <br> 
+You must implement an algorithm that runs in O(n) time and uses constant extra space. <br> 
