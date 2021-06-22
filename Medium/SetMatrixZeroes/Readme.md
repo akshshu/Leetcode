648008122bf883393e4300e8476c9248954086fc
akshshu <akshshu@gmail.com>
2021-06-23 00:24:18 +0530

    setmat0.cpp added

---
+++ Medium/SetMatrixZeroes/Readme.md
@@ -0,0 +1,2 @@
+## Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix. <br> <br> 
+You must do it in place. <br> 
---
+++ Medium/SetMatrixZeroes/setmat0.cpp
@@ -0,0 +1,61 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+        bool rt = false, rc = false;
+        for (int j = 0; j < matrix[0].size(); j++)
+        {
+            if (matrix[0][j] == 0)
+            {
+                rt = true;
+                break;
+            }
+        }
+        for (int j = 0; j < matrix.size(); j++)
+        {
+            if (matrix[j][0] == 0)
+            {
+                rc = true;
+                break;
+            }
+        }
+        for (int i = 0; i < matrix.size(); i++)
+        {
+            for (int j = 0; j < matrix[0].size(); j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.size(); i++)
+            if (matrix[i][0] == 0)
+                for (int j = 0; j < matrix[0].size(); j++)
+                    matrix[i][j] = 0;
+
+        for (int j = 1; j < matrix[0].size(); j++)
+            if (matrix[0][j] == 0)
+                for (int i = 0; i < matrix.size(); i++)
+                    matrix[i][j] = 0;
+
+        if (rt)
+            for (int j = 0; j < matrix[0].size(); j++)
+                matrix[0][j] = 0;
+
+        if (rc)
+            for (int j = 0; j < matrix.size(); j++)
+                matrix[j][0] = 0;
+    }
+};
+int main()
+{
+    Solution s;
+    auto ans = s.func();
+    return 0;
+}
