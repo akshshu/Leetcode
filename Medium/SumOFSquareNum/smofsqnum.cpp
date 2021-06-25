caeadc213c605522378795e50c4dec630620bd91
akshshu <akshshu@gmail.com>
2021-06-26 00:16:16 +0530

    optimised

--- Medium/SumOFSquareNum/smofsqnum.cpp
+++ Medium/SumOFSquareNum/smofsqnum.cpp
@@ -9,38 +9,26 @@ public:
     {
         if (c == 0)
             return true;
-        unordered_map<int, int> cnt;
         while (c % 2 == 0)
-        {
-            cnt[2]++;
             c = c / 2;
-        }
 
         // c must be odd at this point. So we can skip
         // one element (Note i = i +2)
         for (int i = 3; i <= sqrt(c); i = i + 2)
         {
-            // While i divides c, print i and divide c
-            while (c % i == 0)
+            int cnt = 0;
+            if (c % i == 0)
             {
-                cnt[i]++;
-                c = c / i;
-            }
-        }
-
-        // This condition is to handle the case when c
-        // is a prime number greater than 2
-        if (c > 2)
-            cnt[c]++;
-        for (auto x : cnt)
-        {
-            if (x.first % 4 == 3)
-            {
-                if (x.second % 2 != 0)
+                while (c % i == 0)
+                {
+                    cnt++;
+                    c = c / i;
+                }
+                if (cnt % 2 != 0 && i % 4 == 3)
                     return false;
             }
         }
-        return true;
+        return c % 4 != 3;
     }
 };
 int main()
