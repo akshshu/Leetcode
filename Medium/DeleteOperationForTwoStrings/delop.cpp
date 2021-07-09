a8e61218e1a6bd9ffc79a3ae85d13c3c7daad0bf
akshshu <akshshu@gmail.com>
2021-07-09 16:09:04 +0530

    Update delop.cpp

--- Medium/DeleteOperationForTwoStrings/delop.cpp
+++ Medium/DeleteOperationForTwoStrings/delop.cpp
@@ -8,20 +8,22 @@ public:
     {
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        vector<int> dp(n + 1, 0);
         for (int i = 1; i <= m; i++)
         {
+            vector<int> temp(n + 1, 0);
             for (int j = 1; j <= n; j++)
             {
                 if (word1[i - 1] == word2[j - 1])
                 {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    temp[j] = dp[j - 1] + 1;
                     continue;
                 }
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                temp[j] = max(dp[j], temp[j - 1]);
             }
+            dp = temp;
         }
-        return m + n - (2 * dp[m][n]);
+        return m + n - (2 * dp[n]);
     }
 };
 int main()
