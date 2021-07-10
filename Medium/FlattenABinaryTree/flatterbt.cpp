b7259a9478b1d68e33c03b708a31008c676b2544
akshshu <akshshu@gmail.com>
2021-07-10 18:07:18 +0530

    Update flatterbt.cpp

--- Medium/FlattenABinaryTree/flatterbt.cpp
+++ Medium/FlattenABinaryTree/flatterbt.cpp
@@ -7,13 +7,51 @@ public:
     TreeNode *prev = nullptr;
     void flatten(TreeNode *root)
     {
-        if (!root)
-            return root;
-        flatten(root->right);
-        flatten(root->left);
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
+        //   -------------------recursive solution O(n)time O(n)space
+        // if (!root)
+        //     return root;
+        // flatten(root->right);
+        // flatten(root->left);
+        // root->right = prev;
+        // root->left = nullptr;
+        // prev = root;
+        // ---------------------------------------------------
+
+        //----------------------Iterative Solution O(n)time and O(n)space
+        // stack<TreeNode *> st;
+        // st.push(root);
+        // while (!st.empty())
+        // {
+        //     TreeNode *top = st.top();
+        //     st.pop();
+        //     if (root->right)
+        //         st.push(root->right);
+        //     if (root->left)
+        //         st.push(root->left);
+        //     if (!st.empty())
+        //     {
+        //         top->right = st.top();
+        //     }
+        //     top->left = nullptr;
+        // }
+        // ----------------------------------------------------
+
+        // ---------------------Efficient solution O(1)space and O(n)time
+        while (root)
+        {
+            if (root->left)
+            {
+                TreeNode *rightMostLeft = root->left;
+                while (rightMostLeft->right)
+                {
+                    rightMostLeft = rightMostLeft->right;
+                }
+                rightMostLeft->right = root->right;
+                root->right = root->left;
+                root->left = nullptr;
+            }
+            root = root->right;
+        }
     }
 };
 int main()
