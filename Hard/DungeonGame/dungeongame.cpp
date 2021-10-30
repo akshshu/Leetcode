98bbadfd06d96fad6c71c198fb3be218179388c2
Akash Singh Sant <akshshu@gmail.com>
2021-10-30 14:13:03 +0530

    Update dungeongame.cpp

--- Hard/DungeonGame/dungeongame.cpp
+++ Hard/DungeonGame/dungeongame.cpp
@@ -8,70 +8,22 @@ public:
     {
         int n = dungeon.size();
         int m = dungeon[0].size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
-        int x = 0;
-        int reservedDist = dungeon[0][0];
-        if (reservedDist <= 0)
+        vector<vector<int>> dp(n, vector<int>(m));
+        for (int i = n - 1; i >= 0; i--)
         {
-            x = abs(0 - dungeon[0][0]) + 1;
-            reservedDist = x + dungeon[0][0];
-        }
-        int ans = x;
-        pq.push({x, 0, 0, reservedDist});
-        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
-        vector<vector<int>> visited(n, vector<int>(m, 0));
-        dist[0][0] = reservedDist;
-        int ans = INT_MAX;
-        while (!pq.empty())
-        {
-            auto tp = pq.top();
-            pq.pop();
-            if (tp[1] == n - 1 and tp[2] == m - 1)
+            for (int j = m - 1; j >= 0; j--)
             {
-                ans = min(tp[0], ans);
-            }
-            if (visited[tp[1]][tp[2]])
-                continue;
-            visited[tp[1]][tp[2]] = 1;
-            if (tp[1] + 1 < n)
-            {
-                int temp = reservedDist + dungeon[tp[1] + 1][tp[2]];
-                if (temp <= 0)
-                {
-                    int xtoadd = abs(0 - temp) + 1;
-                    if (xtoadd >= dist[tp[1] + 1][tp[2]])
-                        continue;
-                    temp = xtoadd + dungeon[tp[1] + 1][tp[1]];
-                    dist[tp[1] + 1][tp[2]] = xtoadd;
-                    pq.push({xtoadd, tp[1] + 1, tp[2], temp});
-                }
+                if (i == n - 1 and j == m - 1)
+                    dp[i][j] = min(0, dungeon[i][j]);
+                else if (i == n - 1)
+                    dp[i][j] = min(0, dungeon[i][j] + dp[i][j + 1]);
+                else if (j == m - 1)
+                    dp[i][j] = min(0, dungeon[i][j] + dp[i + 1][j]);
                 else
-                {
-                    if (tp[0] >= dist[tp[1] + 1][tp[2]])
-                        continue;
-                    dist[tp[1] + 1][tp[2]] = tp[0];
-                    pq.push({tp[0], tp[1] + 1, tp[2], temp});
-                }
-            }
-            if (tp[2] + 1 < m)
-            {
-                int temp = reservedDist + dungeon[tp[1]][tp[2] + 1];
-                if (temp <= 0)
-                {
-                    int xtoadd = abs(0 - temp) + 1;
-                    if (xtoadd >= dist[tp[1]][tp[2] + 1])
-                        continue;
-                    temp = xtoadd + dungeon[tp[1]][tp[2] + 1];
-                    dist[tp[1]][tp[2] + 1] = xtoadd;
-                    pq.push({xtoadd, tp[1], tp[2] + 1, temp});
-                }
-                else
-                {
-                    dist[tp[1]][tp[2] + 1] = tp[0];
-                    pq.push({tp[0], tp[1], tp[2] + 1, temp});
-                }
+                    dp[i][j] = min(0, dungeon[i][j] + max(dp[i + 1][j], dp[i][j + 1]));
             }
         }
+        return abs(dp[0][0]) + 1;
     }
 };
 int main()
